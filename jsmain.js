43d25b8150accf9a49ad8db6df860c4d19b00dd5
hullale <hullale@oregonstate.edu>
2016-12-02 15:17:31 -0800

    Update jsmain.js

--- jsmain.js
+++ jsmain.js
@@ -1,20 +1,28 @@
 var counter = 0;
-var foods = new Array("meme", "creme", "ice", "baby", "AHHHHHHHH");
+var foods = new Array("1.jpg" , "2.jpg" , "3.jpg"); //put all the images here
 var likedFoods = [];
 
 function iLike() {
 	 foods[counter] = likedFoods[likedFoods.length];
 	 counter ++;
+	 //display foods[counter]
 }
 function iNope() {
 	counter++;
+	//display foods[counter]
 }
 function showFoods() {
 	for(i=0; i<likedFoods.length; i++) {
-		return likedFoods[i];
+		//display foods[counter]
 	}
 }
-userLike.addEventListener("click", iLike);
-userNope.addEventListener("click", iNope);
-userDone.addEventListener("click", showFoods);
+
+var userLike = document.getElementById('likeButton');
+userLike.addEventListener('click', iLike);
+
+var userDislike = document.getElementById('dislikeButton');
+userDislike.addEventListener('click', iNope);
+
+var userDone = document.getElementById('doneButton');
+userDone.addEventListener('click', showFoods);
 
