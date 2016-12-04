16aee2031827c6d67a4277eda7e891553e07b2f0
hullale <hullale@oregonstate.edu>
2016-12-03 17:40:37 -0800

    Update jsmain.js

--- jsmain.js
+++ jsmain.js
@@ -20,7 +20,7 @@ counter++;
 // iLiked function will move images to likedFoods array and bring up new image. 
 function iLike() {
 	console.log("ilike");	
-	var image = document.getElementById("currentImage");
+	//var image = document.getElementById("currentImage");
 	image.src = 'images/' + foods[counter];
 
 	likedFoods[likedFoods.length] = foods[counter]; 
@@ -30,7 +30,7 @@ function iLike() {
 // iNope will remove image and bring up new image.
 function iNope() {
 	console.log("iNope");
-	var image = document.getElementById("currentImage");
+	//var image = document.getElementById("currentImage");
 	image.src = 'images/' + foods[counter];
 	counter++;
 
