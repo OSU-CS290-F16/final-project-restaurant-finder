bd993a30fdcfdd8f445002b676ca083bc4999b6c
hullale <hullale@oregonstate.edu>
2016-12-08 20:43:38 -0800

    stuff

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var personPageTemplate = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+var mainSite = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 app.use(express.static(path.join(__dirname)));
 
 app.get('/data', function(req, res){
-
+  content = mainSite;
+  
   Object.keys(data).forEach(function (restaurant) {
     content += "<img src = " + data[restaurant].fileName + " id = doneImage>";
 
-    var name = document.createElement('p');
-   	name.innerHTML = data[restaurant].Restaurant;
-    div.appendChild(name);
+    //var name = document.createElement('p');
+   	//name.innerHTML = data[restaurant].Restaurant;
+    //div.appendChild(name);
 
-    var description = document.createElement('p');
-   	description.innerHTML = data[restaurant].description;
-    div.appendChild(description);
+    //var description = document.createElement('p');
+   	//description.innerHTML = data[restaurant].description;
+    //div.appendChild(description);
   });
 
   res.send(content);
